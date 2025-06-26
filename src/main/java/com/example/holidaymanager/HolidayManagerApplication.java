1af4365e144dba90af0da1fd8b79b1ad18152a4b
minji-go <kohminji10@gmail.com>
2025-06-26 20:31:58 +0900

    [LS-0] init

---
+++ src/main/java/com/example/holidaymanager/HolidayManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.holidaymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolidayManagerApplication.class, args);
+    }
+
+}
